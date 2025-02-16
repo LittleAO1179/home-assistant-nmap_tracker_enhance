23b41d2768f9ed9a91eaab27bd1ef086e7419380
limingyang <littleao178@outlook.com>
2025-02-16 16:25:51 +0800

    改为surgeapi

--- custom_components/surge_tracker/device_tracker.py
+++ custom_components/surge_tracker/device_tracker.py
@@ -1,110 +1,153 @@
-"""
-使用 arp-scan 扫描网络的支持组件。
-
-有关此平台的更多详细信息，请参阅文档：
-https://github.com/cyberjunky/home-assistant-arpscan_tracker/
+"""使用 Surge API 跟踪设备的组件。
 """
 import logging
-import re
-import subprocess
-from collections import namedtuple
+import json
 from datetime import timedelta
-
+import requests
+from datetime import datetime
+from collections import namedtuple
 import voluptuous as vol
 
 import homeassistant.helpers.config_validation as cv
-import homeassistant.util.dt as dt_util
 from homeassistant.components.device_tracker import (
     DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
+from homeassistant.const import CONF_HOST, CONF_API_KEY
 from homeassistant.util import Throttle
+import homeassistant.util.dt as dt_util
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_EXCLUDE = 'exclude'
 CONF_INCLUDE = 'include'
-CONF_OPTIONS = 'scan_options'
-CONF_NETWORK = 'network'
-DEFAULT_OPTIONS = '-R -sP -PS5353,80,443 -PU5353 --dns-servers 192.168.3.1,8.8.8.8,8.8.4.4 --system-dns'
-DEFAULT_NETWORK = '192.168.1.0/24'
+CONF_EXCLUDE = 'exclude'
+DEFAULT_HOST = '192.168.3.33'
+DEFAULT_PORT = 6171
 
-MIN_TIME_BETWEEN_SCANS = timedelta(seconds=5)
+MIN_TIME_BETWEEN_SCANS = timedelta(seconds=10)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_API_KEY): cv.string,
+    vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string,
+    vol.Optional('port', default=DEFAULT_PORT): cv.port,
     vol.Optional(CONF_INCLUDE, default=[]):
         vol.All(cv.ensure_list, [cv.string]),
     vol.Optional(CONF_EXCLUDE, default=[]):
-        vol.All(cv.ensure_list, [cv.string]),
-    vol.Optional(CONF_OPTIONS, default=DEFAULT_OPTIONS):
-        cv.string,
-    vol.Optional(CONF_NETWORK, default=DEFAULT_NETWORK):
-        cv.string
+        vol.All(cv.ensure_list, [cv.string])
 })
 
 
 def get_scanner(hass, config):
-    """验证配置并返回一个 ArpScan 扫描器。"""
-    return ArpScanDeviceScanner(config[DOMAIN])
+    """验证配置并返回扫描器。"""
+    scanner = SurgeDeviceScanner(config[DOMAIN])
+    return scanner if scanner.success_init else None
 
 Device = namedtuple('Device', ['mac', 'name', 'ip', 'last_update', 'hostname'])
 
-class ArpScanDeviceScanner(DeviceScanner):
-    """此类使用 nmap 扫描设备。"""
-
-    exclude = []
-    include = []
+class SurgeDeviceScanner(DeviceScanner):
+    """使用 Surge API 跟踪设备。"""
 
     def __init__(self, config):
         """初始化扫描器。"""
+        self.host = config.get(CONF_HOST)
+        self.port = config.get('port')
+        self.api_key = config.get(CONF_API_KEY)
+        self.exclude = config.get(CONF_EXCLUDE, [])
+        self.include = config.get(CONF_INCLUDE, [])
+        
         self.last_results = []
+        self.success_init = False
 
-        self.exclude = config[CONF_EXCLUDE]
-        self.include = config[CONF_INCLUDE]
-        self._options = config[CONF_OPTIONS]
-        self._network = config[CONF_NETWORK]
-
-        _LOGGER.debug("正在安装 nmap 软件包")
-        proc = subprocess.Popen('apk add nmap', shell=True, stdin=None, stdout=None, stderr=None, executable="/bin/bash")
-        proc.wait()
-
-        self.success_init = self._update_info()
+        # 测试API连接
+        try:
+            self._update_info()
+            self.success_init = True
+        except requests.exceptions.RequestException as ex:
+            _LOGGER.error("无法连接到 Surge API: %s", ex)
+            self.success_init = False
 
 
     def scan_devices(self):
-        """扫描新设备并返回包含已发现设备ID的列表。"""
+        """扫描设备并返回发现的设备MAC地址列表。"""
         self._update_info()
-
-        _LOGGER.debug("arpscan 最新扫描结果 %s", self.last_results)
-
         return [device.mac for device in self.last_results]
 
 
     def get_device_name(self, mac):
-        """返回给定设备的名称。"""
-        # 对于没有MAC地址的设备，返回其IP地址作为名称
-        if mac.startswith('NO_MAC_'):
-            return mac.replace('NO_MAC_', '').replace('_', '.')
-        return mac.replace(':', '')
-
-
-    def get_extra_attributes(self, device):
-        """返回给定设备的额外属性。"""
-        device_info = next(
-            (result for result in self.last_results if result.mac == device), None
+        """返回设备名称。"""
+        result = next(
+            (device for device in self.last_results if device.mac == mac), None
         )
-        if device_info:
-            attributes = {
-                "ip": device_info.ip,
-                "hostname": device_info.hostname
+        return result.name if result else None
+
+
+    def get_extra_attributes(self, mac):
+        """返回设备的额外属性。"""
+        device = next(
+            (result for result in self.last_results if result.mac == mac), None
+        )
+        if device:
+            return {
+                'ip': device.ip,
+                'hostname': device.hostname,
+                'last_update': device.last_update
             }
-            # 如果是无MAC地址设备，添加标记
-            if device.startswith('NO_MAC_'):
-                attributes["no_mac"] = True
-            return attributes
-        return {"ip": None, "hostname": None}
+        return {}
 
 
     @Throttle(MIN_TIME_BETWEEN_SCANS)
     def _update_info(self):
+        """从 Surge API 更新设备信息。"""
+        _LOGGER.debug("正在从 Surge API 获取设备信息")
+        
+        try:
+            response = requests.get(
+                f'http://{self.host}:{self.port}/v1/devices',
+                headers={'X-Key': self.api_key},
+                timeout=10
+            )
+            response.raise_for_status()
+            data = response.json()
+            
+            now = dt_util.now()
+            self.last_results = []
+
+            for device in data.get('devices', []):
+                # 检查设备是否在 DHCP 设备列表中
+                dhcp_device = device.get('dhcpDevice', {})
+                if not dhcp_device:
+                    continue
+
+                ip = device.get('displayIPAddress')
+                mac = device.get('physicalAddress')
+                hostname = device.get('name')
+                
+                # 检查包含/排除列表
+                if self.include and ip not in self.include:
+                    continue
+                if ip in self.exclude:
+                    continue
+
+                # 检查最后在线时间
+                last_seen = dhcp_device.get('dhcpLastSeen')
+                if last_seen:
+                    try:
+                        last_seen_dt = datetime.strptime(last_seen, '%Y-%m-%dT%H:%M:%S%z')
+                        # 如果设备在5分钟内有活动，就认为在线
+                        if (now.astimezone() - last_seen_dt).total_seconds() < 300:
+                            self.last_results.append(Device(
+                                mac=mac,
+                                name=hostname,
+                                ip=ip,
+                                last_update=last_seen,
+                                hostname=dhcp_device.get('dhcpHostname', '')
+                            ))
+                    except ValueError as e:
+                        _LOGGER.error("解析时间戳错误: %s", e)
+
+            return True
+
+        except requests.exceptions.RequestException as ex:
+            _LOGGER.error("从 Surge API 获取数据失败: %s", ex)
+            return False
         """
         扫描网络中的设备。
         如果扫描成功则返回布尔值。
